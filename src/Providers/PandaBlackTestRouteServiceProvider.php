c15492744e44161e06ae2618cade5825045f3039
sra122 <sravan.k.katta@student.fh-kiel.de>
2018-10-17 12:58:44 +0200

    modification to routes

--- src/Providers/PandaBlackTestRouteServiceProvider.php
+++ src/Providers/PandaBlackTestRouteServiceProvider.php
@@ -14,26 +14,18 @@ class PandaBlackTestRouteServiceProvider extends RouteServiceProvider
      */
     public function map(Router $router, ApiRouter $api)
     {
-        $router->get('hello', 'PandaBlackTest\Controllers\ContentController@productDetails');
-        $router->get('update', 'PandaBlackTest\Controllers\UpdateItemController@updateItems');
-        $router->get('category', 'PandaBlackTest\Controllers\CategoryController@saveCorrelation');
-        $router->get('create-referrer', 'PandaBlackTest\Controllers\ReferrerController@createOrderReferrer');
-        $router->get('referrer', 'PandaBlackTest\Controllers\ReferrerController@getListOfOrderReferrer');
-        $router->get('properties', 'PandaBlackTest\Controllers\ReferrerController@getListOfOrderReferrer');
-        $router->get('expire-time', 'PandaBlackTest\Controllers\AuthController@tokenExpireTime');
-        $router->get('markets/panda-black/attributes', 'PandaBlackTest\Controllers\AttributesController@getAttributes');
+        // Authentication
         $router->get('markets/panda-black/auth/authentication', 'PandaBlackTest\Controllers\AuthController@getAuthentication');
 
         $api->version(['v1'], ['middleware' => ['oauth']], function ($router) {
             $router->get('markets/panda-black/parent-categories', 'PandaBlackTest\Controllers\CategoryController@all');
             $router->get('markets/panda-black/parent-categories/{id}', 'PandaBlackTest\Controllers\CategoryController@get');
             $router->get('markets/panda-black/vendor-categories', 'PandaBlackTest\Controllers\JdCategoriesController@listOfCategories');
-            //$router->get('markets/panda-black/vendor-categories/{id}', 'PandaBlackTest\Controllers\JdCategoriesController@listOfCategories');
             $router->get('markets/panda-black/correlations', 'PandaBlackTest\Controllers\CategoryController@getCorrelations');
             $router->post('markets/panda-black/edit-correlations', 'PandaBlackTest\Controllers\CategoryController@updateCorrelation');
-            $router->post('markets/panda-black/correlations', 'PandaBlackTest\Controllers\CategoryController@saveCorrelation');
-            $router->delete('markets/panda-black/correlations', 'PandaBlackTest\Controllers\CategoryController@deleteAllCorrelations');
-            $router->delete('markets/panda-black/correlation/{id}', 'PandaBlackTest\Controllers\CategoryController@deleteCorrelation');
+            $router->post('markets/panda-black/create-correlation', 'PandaBlackTest\Controllers\CategoryController@saveCorrelation');
+            $router->delete('markets/panda-black/correlations/delete', 'PandaBlackTest\Controllers\CategoryController@deleteAllCorrelations');
+            $router->delete('markets/panda-black/correlation/delete/{id}', 'PandaBlackTest\Controllers\CategoryController@deleteCorrelation');
             $router->get('markets/panda-black/attributes', 'PandaBlackTest\Controllers\AttributesController@getAttributes');
             $router->post('markets/panda-black/attribute', 'PandaBlackTest\Controllers\AttributesController@createAttribute');
             $router->post('markets/panda-black/attribute-mapping', 'PandaBlackTest\Controllers\AttributesController@attributeMapping');
